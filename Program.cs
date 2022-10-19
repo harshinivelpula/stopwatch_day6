835fa66fb511d25bef782b2a39844cbacd56c315
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-19 22:39:08 +0530

    checkstartstop

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+
+        var stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("HI");
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine("Time Elapsed : {0}",
+            stopwatch.Elapsed);
+        Console.ReadLine();
+    }
+}
